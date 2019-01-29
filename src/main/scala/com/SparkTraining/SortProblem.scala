631c2c55ced0279bcae952a89f74a1e28512db27
OFICINA\javier.trapero <javier.trapero@onegolive.com>
2019-01-29 13:24:52 +0100

    Primera version con ejemplo de control de errores

--- src/main/scala/com/SparkTraining/SortProblem.scala
+++ src/main/scala/com/SparkTraining/SortProblem.scala
@@ -2,6 +2,7 @@ package com.SparkTraining
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
+import scala.util.{Try, Success, Failure}
 
 /**
  * Hello world!
@@ -14,6 +15,27 @@ object SortProblem extends App {
   val conf = new SparkConf().setAppName("SortProblem").setMaster("local[*]")
   val sc = new SparkContext(conf)
 
-  val lines = sc.textFile("in/RealEstate.csv")
+  //val lines = sc.textFile("in/RealEstate.csv")
+
+  divide
+
+  def divide: Try[Int] = {
+    val dividend = Try(Console.readLine("Enter an Int that you'd like to divide:\n").toInt)
+    val divisor = Try(Console.readLine("Enter an Int that you'd like to divide by:\n").toInt)
+
+    val problem = dividend.flatMap(x => divisor.map(y => x/y))
+    problem match {
+      case Success(v) =>
+        //println("Result of " + dividend.get + "/"+ divisor.get +" is: " + v)
+        println("Result of " + dividend.get + "/"+ divisor.get +" is: " + v)
+        Success(v)
+      case Failure(e) =>
+        println("You must've divided by zero or entered something that's not an Int. Try again!")
+        println("Info from the exception: " + e.getMessage)
+        divide
+    }
+  }
 
 }
+
+
